92da2fa57aa57e66059489b9d6cdc11b20a7fa10
rpdeore <roshani.pdeore@gmail.com>
2017-02-27 20:19:40 -0800

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-	                                                                'use strict';
+	                                                                
 									var activeIndex = 0;
 									var limit = 0;
 									var disabled = false;
